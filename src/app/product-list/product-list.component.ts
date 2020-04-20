f0cdf4e6045393e7b591a9bad14cf5b6a4a721b4
Elena Tcheprasova <hellogbel@gmail.com>
2020-04-20 18:49:26 +0300

    Working on Product component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -16,26 +16,27 @@ export class ProductListComponent implements OnInit {
     this.initSampleProducts();
   }
 
-  private initSampleProducts(): any {
+  private initSampleProducts(): void {
+    this.productList = new Array<Product>();
     this.productList.push(
       {
         id: 1,
         productName: "Product 1",
         mainFeatures: ["FeatureOne", "FeatureTwo"],
         description: null
-      },
+      } as Product,
       {
         id: 2,
         productName: "Product 2",
         mainFeatures: ["FeatureOne", "FeatureThree"],
         description: null
-      },
+      } as Product,
       {
         id: 3,
         productName: "Product 3",
         mainFeatures: ["FeatureThree", "FeatureFour"],
         description: null
-      }
+      } as Product
     );
   }
 }
