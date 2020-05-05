442b8ee09e6bdd7d82e191ed1e680b285e9233a9
Yaroslav Rogov <rogovyaroslav@gmail.com>
2020-05-05 22:29:54 +0300

    Implement Ingredients Request

--- src/app/ingredients-request/ingredients-request.component.ts
+++ src/app/ingredients-request/ingredients-request.component.ts
@@ -7,6 +7,7 @@ import {Ingredient} from "../api-types";
 import {debounceTime, distinctUntilChanged, map} from "rxjs/operators";
 import {by, element} from "protractor";
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-ingredients-request',
@@ -14,26 +15,28 @@ import {FormBuilder, FormGroup} from "@angular/forms";
   styleUrls: ['./ingredients-request.component.scss']
 })
 export class IngredientsRequestComponent implements OnInit {
-  ingredients: Ingredient[] = []
-  lastComplete: Ingredient[] = []
+  availableIngredients: Ingredient[] = []
   amountOptions = [1, 2, 3, 4, 5]
   countForm: FormGroup;
 
-  public ingredientName: string = "";
+  ingredientList: Array<Ingredient>;
+  correctInput = false
 
-  selected: Number[] = []
-  public amountValues: Array<number> = Array.from(Array(10).keys());
-  public ingredientList: Array<Ingredient>;
-  public currentIngredient: Ingredient;
+  constructor(
+    private api: GraphqlService,
+    private fb: FormBuilder,
+    private router: Router,
+    private toaster: ToastrService) {}
 
-  constructor(private api: GraphqlService, private fb: FormBuilder, private router: Router) {}
   ngOnInit() {
-    this.ingredientList = new Array<Ingredient>();
-    this.ingredientList.push({} as Ingredient);
-      this.api.getAllIngredients()
-        .subscribe(data => this.ingredients = data)
+    this.api.getAllIngredients()
+      .subscribe(data => this.availableIngredients = data)
+
+    const defaultCount = this.amountOptions[0]
+    this.ingredientList = [{count: defaultCount} as Ingredient];
+
     this.countForm = this.fb.group({
-      countControl: [ 1 ]
+      countControl: [ defaultCount ]
     });
   }
 
@@ -41,24 +44,15 @@ export class IngredientsRequestComponent implements OnInit {
     text.pipe(
       debounceTime(200),
       distinctUntilChanged(),
-      map(term => {
-        let result =
-          term.length < 2 ? []:
-            this.ingredients
-              .filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1)
-              .slice(0, 10)
-
-        this.lastComplete = result
-        return result.map(v => v.name)
-      })
-    )
+      map(term =>
+        this.availableIngredients
+          .filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1)
+          .slice(0, 10)
+          .map(v => v.name)))
 
   public add(): void {
-    const length = this.ingredientList.length;
-    const amount = this.countForm.controls.countControl.value;
-    this.currentIngredient.count = amount;
-    this.ingredientList.pop();
-    this.ingredientList.push(this.currentIngredient, {} as Ingredient);
+    this.ingredientList.push({count: 1} as Ingredient)
+    this.correctInput = false
   }
 
   public remove(idx: number): void {
@@ -70,15 +64,30 @@ export class IngredientsRequestComponent implements OnInit {
   }
 
   public request() {
-    this.api.createIngredientRequest(this.ingredientList.map(s => s.id));
+    let ingredients = []
+
+    this.ingredientList
+      .slice(0, -1)
+      .forEach(e => {
+        ingredients.length += e.count
+        ingredients.fill(e.id, ingredients.length - e.count, ingredients.length)
+      })
+
+    this.api.createIngredientRequest({ingredients: ingredients})
+      .subscribe(id => this.toaster.success("Request (id " + id + ") was created"));
+  }
+
+  public changeCount(index, count) {
+    this.ingredientList[index].count = Number.parseInt(count)
   }
 
   validateInput(s: String) {
-    let l = this.lastComplete.filter(v => v.name == s)
-    if (l.length != 1) console.error("Eblan");
-    else {
-      console.log(l[0])
-      this.currentIngredient = l[0];
+    let ingredient = this.availableIngredients.filter(v => v.name == s)[0]
+
+    if (name !== undefined) {
+      this.ingredientList[this.ingredientList.length-1].id = ingredient.id
+      this.ingredientList[this.ingredientList.length-1].name = ingredient.name
+      this.correctInput = true
     }
   }
 }
