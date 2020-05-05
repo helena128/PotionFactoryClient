c395ce375dd57ad2435224f9fdefb1bf3b4b2a66
Elena <hellogbel@gmail.com>
2020-05-05 12:21:28 +0300

    Working on adding ingredients to request

--- src/app/ingredients-request/ingredients-request.component.ts
+++ src/app/ingredients-request/ingredients-request.component.ts
@@ -6,6 +6,7 @@ import {Observable} from "rxjs";
 import {Ingredient} from "../api-types";
 import {debounceTime, distinctUntilChanged, map} from "rxjs/operators";
 import {by, element} from "protractor";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-ingredients-request',
@@ -15,14 +16,25 @@ import {by, element} from "protractor";
 export class IngredientsRequestComponent implements OnInit {
   ingredients: Ingredient[] = []
   lastComplete: Ingredient[] = []
+  amountOptions = [1, 2, 3, 4, 5]
+  countForm: FormGroup;
+
+  public ingredientName: string = "";
 
   selected: Number[] = []
   public amountValues: Array<number> = Array.from(Array(10).keys());
+  public ingredientList: Array<Ingredient>;
+  public currentIngredient: Ingredient;
 
-  constructor(private api: GraphqlService) {}
+  constructor(private api: GraphqlService, private fb: FormBuilder, private router: Router) {}
   ngOnInit() {
+    this.ingredientList = new Array<Ingredient>();
+    this.ingredientList.push({} as Ingredient);
       this.api.getAllIngredients()
         .subscribe(data => this.ingredients = data)
+    this.countForm = this.fb.group({
+      countControl: [ 1 ]
+    });
   }
 
   search = (text: Observable<string>) =>
@@ -41,9 +53,28 @@ export class IngredientsRequestComponent implements OnInit {
       })
     )
 
+  public add(): void {
+    const length = this.ingredientList.length;
+    const amount = this.countForm.controls.countControl.value;
+    this.currentIngredient.count = amount;
+    this.ingredientList.pop();
+    this.ingredientList.push(this.currentIngredient, {} as Ingredient);
+  }
+
+  public remove(idx: number): void {
+    this.ingredientList.splice(idx, 1);
+  }
+
+  public redirectToDetailsPage(id: number): void {
+    this.router.navigate(['/ingredients/:id', id]);
+  }
+
   validateInput(s: String) {
     let l = this.lastComplete.filter(v => v.name == s)
-    if (l.length != 1) alert("Eblan")
-    else console.log(l[0])
+    if (l.length != 1) console.error("Eblan");
+    else {
+      console.log(l[0])
+      this.currentIngredient = l[0];
+    }
   }
 }
