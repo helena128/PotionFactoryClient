3ac7196cf089509f1ea45f03c3c5cdd2306b28ef
Elena <hellogbel@gmail.com>
2020-05-05 14:19:42 +0300

    Manufature report: Working on business logic

--- src/app/manufacture-report/manufacture-report.component.ts
+++ src/app/manufacture-report/manufacture-report.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {ManufactureReportItem} from "../model/manufacture-report-item";
+import {Component, OnInit} from '@angular/core';
+import {Product} from "../api-types";
+import {Observable} from "rxjs";
+import {debounceTime, distinctUntilChanged, map} from "rxjs/operators";
+import {GraphqlService} from "../graphql.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-manufacture-report',
@@ -8,35 +13,67 @@ import {ManufactureReportItem} from "../model/manufacture-report-item";
 })
 export class ManufactureReportComponent implements OnInit {
 
-  public amountValues: Array<number>;
-  public manufacturedItemList: Array<ManufactureReportItem>;
+  products: Product[] = []
+  lastComplete: Product[] = []
+  currentProduct: Product;
+  countForm: FormGroup;
 
-  constructor() { }
+  public amountValues = [1, 2, 3, 4, 5];
+  public manufacturedItemList: Array<Product> = [];
+
+  constructor(private apiService: GraphqlService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
-    this.initAmountValues();
-    this.initManufacturedItemList();
+    this.manufacturedItemList.push({} as Product);
+    this.apiService.searchProducts().subscribe((data) => this.products = data);
+    this.countForm = this.fb.group({
+      countControl: [ 1 ]
+    });
   }
 
-  private initManufacturedItemList(): void {
-    this.manufacturedItemList = new Array<ManufactureReportItem>();
-    this.manufacturedItemList.push(
-      {
-        potionName: 'Potion One',
-        amount: 3
-      },
-      {
-        potionName: 'Potion Two',
-        amount: 4
-      },
-      {
-        potionName: 'Potion Three',
-        amount: 2
-      }
-    )
+  search = (text: Observable<string>) =>
+    text.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => {
+        let result =
+          term.length < 2 ? []:
+            this.products
+              .filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1)
+              .slice(0, 10)
+
+        this.lastComplete = result
+        return result.map(v => v.name)
+      })
+    );
+
+  public add(): void {
+    const amount = this.countForm.controls.countControl.value;
+    this.currentProduct.count = amount;
+    this.manufacturedItemList.pop();
+    this.manufacturedItemList.push(this.currentProduct, {} as Product);
+    console.log(this.manufacturedItemList);
   }
 
-  private initAmountValues(): void {
-    this.amountValues = Array.from(Array(10).keys());
+  public remove(idx: number): void {
+    this.manufacturedItemList.splice(idx, 1);
+  }
+
+  public navigate(id: number): void {
+    console.log('ID:', id);
+    this.router.navigate(['products', id]);
+  }
+
+  sendRequest() {
+    // TODO
+  }
+
+  validateInput(s: String) {
+    let l = this.lastComplete.filter(v => v.name == s)
+    if (l.length != 1) console.error("Eblan");
+    else {
+      console.log(l[0])
+      this.currentProduct = l[0];
+    }
   }
 }
