fdc6e855bd9a13ff6aed70255d68258dc4806d07
Yaroslav Rogov <rogovyaroslav@gmail.com>
2020-10-27 11:31:41 +0300

    feat: Add network error handling

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -2,15 +2,43 @@ import {NgModule} from '@angular/core';
 import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
+import { onError } from 'apollo-link-error';
+import {from} from "apollo-link";
+import {ToastrService} from "ngx-toastr";
 
 const uri = "/graphql"; // <-- add the URL of the GraphQL server here
 
-export function createApollo(httpLink: HttpLink) {
+export function createApollo(httpLink: HttpLink, toastr: ToastrService) {
+  const link =
+    from(
+      [
+        onError((r) => {
+          if (r.networkError) {
+            var message;
+            if (r.networkError['statusText']) {
+              switch (r.networkError['statusText']) {
+                case 'Unknown Error':
+                  message = "Connection Error"; break
+                default:
+                  message = r.networkError['statusText']
+              }
+            }
+            else {
+              message = r.networkError.message
+            }
+
+            toastr.error("Network Error: " + message)
+          }
+        }),
+        httpLink.create({
+          uri: uri,
+          withCredentials: true
+        })
+      ]
+    )
+
   return {
-    link: httpLink.create({
-      uri: uri,
-      withCredentials: true
-    }),
+    link,
     cache: new InMemoryCache(),
     credentials: 'include',
     defaultOptions: {
@@ -36,7 +64,7 @@ export function createApollo(httpLink: HttpLink) {
     {
       provide: APOLLO_OPTIONS,
       useFactory: createApollo,
-      deps: [HttpLink],
+      deps: [HttpLink, ToastrService],
     },
   ],
 })
