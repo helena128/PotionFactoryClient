e8f680a47a1ea5d33f1c42c4cb7add90e0ab2b58
helena128 <hellogbel@gmail.com>
2020-10-27 22:37:36 +0300

    Added basic functionality of adding ingredients

--- src/app/recipe-form/recipe-form.component.ts
+++ src/app/recipe-form/recipe-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Ingredient} from "../api-types";
+import {Ingredient, UserRole} from "../api-types";
 import {GraphqlService} from "../graphql.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-recipe-form',
@@ -9,12 +10,42 @@ import {GraphqlService} from "../graphql.service";
 })
 export class RecipeFormComponent implements OnInit {
 
-  ingredientList: any[];
+  availableIngredients: any[];
+  chosenIngredients: any[];
+  userRoleGroup: FormGroup;
+  currentIngredient;
+  description: string;
 
-  constructor(private graphQlService: GraphqlService) { }
+  constructor(private graphQlService: GraphqlService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.graphQlService.getAllIngredients().subscribe(data => this.ingredientList = data);
+    this.graphQlService.getAllIngredients().subscribe(data => this.availableIngredients = data.map(ingr => ingr.name));
+    this.chosenIngredients = [''];
+    this.userRoleGroup = this.fb.group({
+      userRoleControl: [ {name: ''} ]
+    });
+    this.userRoleGroup.valueChanges.subscribe(x => this.currentIngredient = x);
+    this.description = '';
   }
 
+  handleCurrentIngredientChange(event): void {
+    this.currentIngredient = event;
+  }
+
+  remove(idx: number): void {
+    if (idx >= 0 && idx < this.chosenIngredients.length) {
+      this.chosenIngredients.splice(idx, 1);
+      console.debug('Remove: ', this.chosenIngredients);
+    }
+    if (this.chosenIngredients.length === 0) {
+      this.chosenIngredients.push('');
+    }
+  }
+
+  add(): void {
+    if (this.currentIngredient) {
+      this.chosenIngredients.splice(this.chosenIngredients.length - 1, 0, this.currentIngredient);
+      console.debug('Add: ', this.chosenIngredients);
+    }
+  }
 }
