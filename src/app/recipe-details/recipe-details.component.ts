eeb0a2af5d6c74868e1935236b7ded32822e05b0
helena128 <hellogbel@gmail.com>
2020-10-29 22:24:04 +0300

    Recipe details component enhancement

--- src/app/recipe-details/recipe-details.component.ts
+++ src/app/recipe-details/recipe-details.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {Recipe} from "../api-types";
+import {Component, OnInit} from '@angular/core';
 import {GraphqlService} from "../graphql.service";
 import {ActivatedRoute} from "@angular/router";
 
@@ -12,26 +11,17 @@ export class RecipeDetailsComponent implements OnInit {
   recipe: any;
 
   constructor(private graphqlService: GraphqlService,
-              private activatedRoute: ActivatedRoute) { }
+              private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
-    /*this.recipe = {
-      id: 1,
-      name: 'Demo Recipe',
-      description: 'Small description of recipe',
-      tags: ['Ingredient1', 'Ingredient2']
-    };*/
     const id = Number.parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
-    if (!id) {
-      console.error("EXCEPTION");
-    } else {
-      this.graphqlService.getRecipeById(id).subscribe(data => this.recipe = {
-        id: data.id,
-        name: data.name,
-        description: data.description,
-        tags: data.ingredients.map(ingredient => ingredient.name)
-      });
-    }
+    this.graphqlService.getRecipeById(id).subscribe(data => this.recipe = {
+      id: data.id,
+      name: data.name,
+      description: data.description,
+      tags: data.ingredients.map(ingredient => ingredient.name) // TODO: make unique
+    });
   }
 
 }
--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Ingredient, Recipe, UserRole} from "../api-types";
 import {GraphqlService} from "../graphql.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-list',
@@ -12,7 +13,8 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
   createRecipeButtonCaption = 'Create recipe';
 
-  constructor(private graphQlService: GraphqlService) { }
+  constructor(private graphQlService: GraphqlService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.graphQlService.getAllRecipes().subscribe(data => this.recipes = data);
@@ -24,5 +26,9 @@ export class RecipeListComponent implements OnInit {
 
   canCreateRecipe(): boolean {
     return localStorage.getItem('userRole') === UserRole.Fairy;
+  }
+
+  viewRecipe(id: number) {
+    this.router.navigate(['recipes', id]);
   }
 }
